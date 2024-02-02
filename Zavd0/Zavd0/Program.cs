6ecdc1a930fb8bfe073c21c7b4648a1ed26d7002
Dmytro Palamarchyk <ipz224_pdo@student.ztu.edu.ua>
2024-02-02 17:37:49 +0200

    add gitignore

---
+++ Zavd0/Zavd0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть перше число:");
+     
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+ 
+        double num2 = Convert.ToDouble(Console.ReadLine());
+      
+        double sum = num1 + num2;
+        
+        Console.WriteLine($"Сума чисел {num1} та {num2} дорівнює {sum}");
+
+        Console.ReadLine();
+    }
+}
